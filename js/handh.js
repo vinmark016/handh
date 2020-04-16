d2f5e43232a4c9638e869180550f0502ee6e36d0
vinmark016 <Icebp86ga30u>
2020-04-16 22:20:32 +0800

    Update chg

--- js/handh.js
+++ js/handh.js
@@ -36,14 +36,19 @@ var mark = {
 
                         var html = '';
                         var t = $.parseJSON(e);
+                        var color = "#E91E63";
                         $.each(t,function(key, val) {
+
+                            if(val.chg > 0) {
+                                color = "#4CAF50";
+                            }
 
                             html += '<tr>';
                             html += `   <td>${val.delivery}</td>`;
                             html += `   <td>${val.bid}</td>`;
                             html += `   <td>${val.basis}</td>`;
                             html += `   <td>${val.futures}</td>`;
-                            html += `   <td style="color:#900;">${val.chg}</td>`;
+                            html += `   <td style="font-weight:bold; color:${color};">${val.chg}</td>`;
                             html += `   <td>${val.symbol}</td>`;
                             html += `   <td>${val.last_trade}</td>`;
                             html += '</tr>';
