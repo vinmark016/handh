559e81e6b33679be4eb35556a42a0551c003642e
vinmark016 <Icebp86ga30u>
2020-05-19 22:52:39 +0800

    Update

--- js/handh.js
+++ js/handh.js
@@ -17,6 +17,13 @@ var mark = {
 
 			})
 
+			$("#bushels_text").keyup(function() {
+
+				var val = $(this).val();
+				val = val.replace(",","");
+				$(this).val(mark.numberFormat(val));
+
+			});
 
       data = {
            action: 'readBasis',
@@ -26,7 +33,29 @@ var mark = {
        mark.ajaxCall(data, "POST", true);
 
     },
-
+		numberFormat: function(number, decimals, dec_point, thousands_sep) {
+		    // Strip all characters but numerical ones.
+		    number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
+		    var n = !isFinite(+number) ? 0 : +number,
+		        prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+		        sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+		        dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+		        s = '',
+		        toFixedFix = function (n, prec) {
+		            var k = Math.pow(10, prec);
+		            return '' + Math.round(n * k) / k;
+		        };
+		    // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+		    s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+		    if (s[0].length > 3) {
+		        s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+		    }
+		    if ((s[1] || '').length < prec) {
+		        s[1] = s[1] || '';
+		        s[1] += new Array(prec - s[1].length + 1).join('0');
+		    }
+		    return s.join(dec);
+		},
     ajaxCall: async function(data,method,async) {
 
         $.ajax({
